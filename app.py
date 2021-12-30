bedaee07e0618053a6ba0e512845ce1620f792f2
all4me7 <m.wrzochol@gmail.com>
2021-12-30 13:38:16 +0100

    master

--- app.py
+++ app.py
@@ -56,6 +56,18 @@ def main():
                     scraped_tags = soup.find_all(tag, attrs={atr: True})
                     for single_tag in scraped_tags:
                         list_tag.append(single_tag[atr])
+                elif atr.endswith('#'):
+                    atr = atr.removesuffix('#')
+                    scraped_tags = [x[atr] for x in soup.find_all(tag, attrs={atr: True})]
+                    filtered_content = [x.strip() for x in scraped_tags if x.startswith("http") or x.startswith("/")]
+                    filtered_content = list(set(filtered_content))
+                    if len(filtered_content) > 0:
+                        for link in filtered_content:
+                            if link.startswith("/"):
+                                link = protocol + domain_name + link
+                                list_tag.append(link)
+                            else:
+                                list_tag.append(link)
                 elif atr.endswith('!'):
                     atr = atr.removesuffix('!')
                     scraped_tags = [x[atr] for x in soup.find_all(tag, attrs={atr: True})]
@@ -72,6 +84,8 @@ def main():
                             
                             if status_code == 404:
                                 list_tag.append(link)
+                    else:
+                        list_tag.append("Dead links not found.")
                 elif atr.endswith('*'):
                     atr = atr.removesuffix('*')
                     scraped_tags = soup.find_all(tag, attrs={atr: True})
