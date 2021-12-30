02cb666e78848edacdfa2d16dca42b84b4f223e8
all4me7 <m.wrzochol@gmail.com>
2021-12-30 13:06:03 +0100

    master

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from requests.exceptions import ConnectionError
 import requests as req
 
 
@@ -14,6 +16,8 @@ def home():
 def main():
     # VARIABLES
     list_tag = []
+    protocol = "https://"
+    temp = []
 
     # GET VALUES FROM HTML 
     if request.method == 'POST':
@@ -24,10 +28,10 @@ def main():
             val = request.form['value']
 
             # FIXING URL'S 
-            if url.startswith('https://'):
-                pass
-            else:
-                url = 'https://' + url
+            if url.startswith(protocol) is not True:
+                url = protocol + url
+
+            domain_name = urlparse(url).netloc
 
             # SOUP SETUP
             response = req.get(url)
@@ -52,6 +56,25 @@ def main():
                     scraped_tags = soup.find_all(tag, attrs={atr: True})
                     for single_tag in scraped_tags:
                         list_tag.append(single_tag[atr])
+                elif atr.endswith('!'):
+                    atr = atr.removesuffix('!')
+                    scraped_tags = [x[atr] for x in soup.find_all(tag, attrs={atr: True})]
+                    filtered_content = [x.strip() for x in scraped_tags if x.startswith("http") or x.startswith("/")]
+                    filtered_content = list(set(filtered_content))
+                    
+                    if len(filtered_content) > 0:
+                        for link in filtered_content:
+                            if link.startswith("/"):
+                                link = protocol + domain_name + link
+                            try:
+                                status_code = req.get(link, timeout=4).status_code
+                            except:
+                                continue
+                            
+                            if status_code == 404:
+                                list_tag.append(link)
+                        
+
                 elif atr.endswith('*'):
                     atr = atr.removesuffix('*')
                     scraped_tags = soup.find_all(tag, attrs={atr: True})
